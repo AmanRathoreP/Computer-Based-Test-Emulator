ff9ef808487d5147dc1d99d29997923066343648
Aman <aman.proj.rel@gmail.com>
2024-05-01 13:38:14 +0530

    change the behavior of warning and threshold time
    this is done to make sure that if threshold time is set to 10sec, when exam timer shows 10sec the colour should be changed to red etc.

--- src/frames/exam_frame.cpp
+++ src/frames/exam_frame.cpp
@@ -77,11 +77,11 @@ void examFrame::OnTimer(wxTimerEvent &event)
     this->exam_timer_display->SetLabel(formatted_exam_time());
 
     // Check if the specific time is reached
-    if (this->exam_remaining_time <= this->test_starting_data.warning_time)
+    if (this->exam_remaining_time <= this->test_starting_data.warning_time + 1)
     {
         this->exam_timer_display->SetForegroundColour(wxColor(154, 133, 48));
     }
-    if (this->exam_remaining_time <= this->test_starting_data.threshold_time)
+    if (this->exam_remaining_time <= this->test_starting_data.threshold_time + 1)
     {
         this->exam_timer_display->SetForegroundColour(wxColor(255, 40, 40));
     }
